650125f2f34f5542f29f61df5c62b9537d3c6411
panda-sas <panda.sas20@gmail.com>
2021-11-01 17:48:36 -0500

    Random turtle walk

--- RandomWalk/main.py
+++ RandomWalk/main.py
@@ -2,14 +2,22 @@ import random
 import turtle as t
 
 tim = t.Turtle()
+t.colormode(255)
 
-colors = ["MediumBlue", "ForestGreen", "MediumSpringGreen", "Brown", "Coral", "PeachPuff", "Chocolate", "GreenYellow",
-          "LightCoral", "Gold", "Crimson", "Magenta", "BlueViolet"]
+
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    random_colour = (r , g, b)
+    return random_colour
+
+
 directions = [0, 90, 180, 270]
 tim.pensize(15)
 tim.speed("fastest")
 for _ in range(200):
-    tim.color(random.choice(colors))
+    tim.color(random_color())
     tim.forward(30)
     tim.setheading(random.choice(directions))
 
